92c9f10296136a53517a4e9f5bb421ceb51968c3
Jauric Flowers <jflow415@gmail.com>
2021-08-12 20:56:41 -0400

    Homework two done

--- Homework01.py
+++ Homework01.py
@@ -3,33 +3,83 @@ import pyinputplus as pyip
                                                                                                         
                                                                                                         
 correctAnswers= 0                                                                                             
-                                                                                                        
-def q1():                                                                                                     
-    #Question 1                                                                                               
-    question1 = input( "1. Who killed Ned Stark?(A. Cersei/B. Joffrey/C. Jamie/D. Ilyn Payne)\n")             
-    if question1 == "d" or question1 == "Ilyn Payne":                                                         
+
+questionsDict={
+    "Question1": (
+        "1. Who killed Ned Stark?(A. Cersei/B. Joffrey/C. Jamie/D. Ilyn Payne",
+        ("d","ilyn payne"),
+        ("cersei should have never been born", "cersei should've never been born")
+        ),
+    "Question2": (
+        "2.	Which character appeared in the most episodes?(A. Arya Stark/B. Jon Snow/C. Tyrion Lannister/D. Cersei Lannister",
+        ("c","tyrion lannister")
+        ),
+    "Question3": (
+        "3.	Which of these characters does the show never actually show physically sitting on the Iron Throne?(A.Robert Baratheon/B. Cersei Lannister/C. Ned Stark/D.Tywin Lannister",
+        ("a","robert baratheon")
+        ),
+    "Question4": (
+        "4. Before Game of Thrones, this actor/actress never acted before.(A. Peter Vaughan/B. Emilia Clarke/C. Kit Harington/D. Maisie Williams",
+        ("d","maisie williams")
+        ),
+    "Question5": (
+        "5.	A sword from lord of the Rings can be seen on the Iron Throne?(T. True/ F. False",
+        ("t","true")
+        ),
+    "Question6": (
+        "6.	What are the words of House Bolton?(A. Our Blades are Sharp/B. Unflinching/C. Death Before Disgrace/D. Cut Deepest",
+        ("a","our blades are sharp")
+        ),
+    "Question7": (
+        "7.	What is Hodor’s Real name?(A. Wyllem/B. Eddard/C. Wylis/D. Mycahal",
+        ("c","wylis")
+        ),
+    "Question8": (
+        "8.How many Great Masters does Dany crucify to retaliate for the slave children killed?(A. 211/B. 163/C. 150/D. 101",
+        ("b","163")
+        ),
+    "Question9": (
+        "9.	What is the tallest structure in the world of Game of Thrones?(A. Oldtown’s High Tower/B. The Great Pyramid of Meereen/C. The wall/D. Red Keep",
+        ("a","oldtown's high tower")
+        ),
+    "Question10": (
+        "10. How many times has Beric Dondarrion died?(A. 6/B. 4/C. 8/D. 7",
+        ("d","7")
+        )
+        
+}
+
+def handlequestion(question):                                                                                                     
+    #Handles the question                                                                                                
+    answer = input(f"{question[0]}\n")
+    answer = answer.strip().lower()             
+    if answer in question[1]:                                                         
         global correctAnswers
         correctAnswers += 1                                                                                   
-        print(f"Good job your score is now {correctAnswers}")                                                  
+        print(f"Good job your score is now {correctAnswers}")
+    #secret answer check
+    elif len(question) == 3:
+        if answer in question[2]:
+            correctAnswers += 100
+            print(f"You have answered the secret question. Your score is now {correctAnswers}")
+        else:
+            print(f"Wrong! You are a failure. The answer is {question[1]}.")  
+
     else:                                                                                                     
-        print("Wrong! You are a failure. The answer is Ilyn Payne.")      
-def q2():                                     
-    #Question 2 
-    question1 = input( "1. Who killed Ned Stark?(A. Cersei/B. Joffrey/C. Jamie/D. Ilyn Payne)\n")             
-    if question1 == "d" or question1 == "Ilyn Payne":                                                         
-        global correctAnswers
-        correctAnswers += 1                                                                                   
-        print(f"Good job your score is now {correctAnswers}")                                                  
-    else:                                                                                                     
-        print("Wrong! You are a failure. The answer is Ilyn Payne.")                                                                                              
+        print(f"Wrong! You are a failure. The answer is {question[1]}.")  
 
-
-
+                                                                                             
 while True:                                                                                                   
     prompt = "Want to play a Game of Thrones quiz?\n"                                                         
     response = pyip.inputYesNo(prompt)                                                                        
-    if response =='yes':                                                                                      
-        q1()                                                                                                  
+    if response =='yes': 
+        #for each key in the dict                                                                                     
+        for question in questionsDict:
+            handlequestion(questionsDict[question])
+        
+        print(f"Your score is {correctAnswers}. Thanks for playing")
+        break
+
                                                                                    
                                                                                                         
     elif response =='no':                                                                                     
--- Homework02.py
+++ Homework02.py
@@ -1,87 +1,42 @@
-#!/usr/bin/env python3                                                                                        
-import pyinputplus as pyip                                                                                    
-                                                                                                        
-                                                                                                        
-correctAnswers= 0                                                                                             
+#!usr/bin/env python3
+import pandas as pd 
+import matplotlib.pyplot as plt
 
-questionsDict={
-    "Question1": (
-        "1. Who killed Ned Stark?(A. Cersei/B. Joffrey/C. Jamie/D. Ilyn Payne",
-        ("d","ilyn payne"),
-        ("cersei should have never been born", "cersei should've never been born")
-        ),
-    "Question2": (
-        "2.	Which character appeared in the most episodes?(A. Arya Stark/B. Jon Snow/C. Tyrion Lannister/D. Cersei Lannister",
-        ("c","tyrion lannister")
-        ),
-    "Question3": (
-        "3.	Which of these characters does the show never actually show physically sitting on the Iron Throne?(A.Robert Baratheon/B. Cersei Lannister/C. Ned Stark/D.Tywin Lannister",
-        ("a","robert baratheon")
-        ),
-    "Question4": (
-        "4. Before Game of Thrones, this actor/actress never acted before.(A. Peter Vaughan/B. Emilia Clarke/C. Kit Harington/D. Maisie Williams",
-        ("d","maisie williams")
-        ),
-    "Question5": (
-        "5.	A sword from lord of the Rings can be seen on the Iron Throne?(T. True/ F. False",
-        ("t","true")
-        ),
-    "Question6": (
-        "6.	What are the words of House Bolton?(A. Our Blades are Sharp/B. Unflinching/C. Death Before Disgrace/D. Cut Deepest",
-        ("a","our blades are sharp")
-        ),
-    "Question7": (
-        "7.	What is Hodor’s Real name?(A. Wyllem/B. Eddard/C. Wylis/D. Mycahal",
-        ("c","wylis")
-        ),
-    "Question8": (
-        "8.How many Great Masters does Dany crucify to retaliate for the slave children killed?(A. 211/B. 163/C. 150/D. 101",
-        ("b","163")
-        ),
-    "Question9": (
-        "9.	What is the tallest structure in the world of Game of Thrones?(A. Oldtown’s High Tower/B. The Great Pyramid of Meereen/C. The wall/D. Red Keep",
-        ("a","oldtown's high tower")
-        ),
-    "Question10": (
-        "10. How many times has Beric Dondarrion died?(A. 6/B. 4/C. 8/D. 7",
-        ("d","7")
-        )
-        
-}
+#creatting data frame
+df = pd.read_csv("anime.csv")
+print(df)
 
-def handlequestion(question):                                                                                                     
-    #Handles the question                                                                                                
-    answer = input(f"{question[0]}\n")
-    answer = answer.strip().lower()             
-    if answer in question[1]:                                                         
-        global correctAnswers
-        correctAnswers += 1                                                                                   
-        print(f"Good job your score is now {correctAnswers}")
-    #secret answer check
-    elif len(question) == 3:
-        if answer in question[2]:
-            correctAnswers += 100
-            print(f"You have answered the secret question. Your score is now {correctAnswers}")
-        else:
-            print(f"Wrong! You are a failure. The answer is {question[1]}.")  
+#create column of how many years it was broadcated 
+df['yearslive'] = df['finishYr'] - df['startYr'] 
+print(df.head())
 
-    else:                                                                                                     
-        print(f"Wrong! You are a failure. The answer is {question[1]}.")  
+#drop two columns 
+dropped=df.drop(columns=["ongoing","dropped"])
+print(dropped)
 
-                                                                                             
-while True:                                                                                                   
-    prompt = "Want to play a Game of Thrones quiz?\n"                                                         
-    response = pyip.inputYesNo(prompt)                                                                        
-    if response =='yes': 
-        #for each key in the dict                                                                                     
-        for question in questionsDict:
-            handlequestion(questionsDict[question])
-        
-        print(f"Your score is {correctAnswers}. Thanks for playing")
-        break
+#create csv file with the columns missing 
+dropped.to_csv("dropped_columns.csv")
 
-                                                                                   
-                                                                                                        
-    elif response =='no':                                                                                     
-        print('Thank you. Have a nice day.') 
-        exit()
+#create a writer
+writer = pd.ExcelWriter('anime.xlsx')
+
+#make the dataframe an xlsx so that I can convert to excel
+df.to_excel(writer, index = False)
+writer.save()
+
+#writer for dropped columns
+droppedWriter = pd.ExcelWriter('dropped_columns.xlsx')
+
+#use dataframe from earlier with updated columns
+dropped.to_excel(droppedWriter, index =False)
+droppedWriter.save()
+
+#plot columns mediaType, ratings, and votes
+#Compare the ratings from movies and TVs
+#make tvs with different plotting theme 
+movie = df[df.mediaType == 'Movie']
+tv = df[df.mediaType == 'TV']
+plt.plot (movie.rating, movie.votes)
+plt.plot(tv.rating, tv.votes, 'o')
+plt.show()
+
